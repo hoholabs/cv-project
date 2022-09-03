c95184be6dd0e8d6b5e14121cd26afb2b04d646d
hoholabs <johnpaulhoran@gmail.com>
2022-09-03 16:02:28 -0400

    Now we can add educations

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,31 +6,49 @@ class Education extends Component{
     constructor(){
         super()
         this.state = {
-            cards:[
-                {
-                school:'test school',
-                program:'map program',
-                dates:'dates',
-                key:1,
-                },
-                {
-                    school:'test school',
-                    program:'map program',
-                    dates:'dates2',
-                    key:2,
-                    }
-
-            ],
+            cards:[],
             card:{
-                school:'school',
-                program:'program',
-                dates:'dates',
-                key:new Date().getTime(),
+                school: '',
+                program: '',
+                dates: '',
+                key: '',
             },
             editing: false,
-
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange = (event) =>{
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            card:{
+                ...this.state.card,
+                [name]: value,
+                key:new Date().getTime(),
+            }
+        })
+    }
+
+    handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log(this.state.cards);
+        this.setState(prevState =>({
+            cards:[...prevState.cards, this.state.card],
+            card:{
+                school:'',
+                program:'',
+                dates:'',
+                key:new Date().getTime(),
+            }
+        }))
+        console.log(this.state.card);
+    }
+
     render (){
 
         return  <section id = 'education'>  
@@ -39,18 +57,36 @@ class Education extends Component{
 
                     {/* returns all of the education cards, using the state cards */}
                     {this.state.cards.map((card) =>{
-			            return  <div className = 'education-card'>    
-                                    <EducationCard school={card.school} program={card.program} dates={card.dates} key={card.key}/>
-                                    <button className = 'material-icon'>edit</button> 
+			            return  <div key = {card.key+'-container'} className = 'education-card'>    
+                                    <EducationCard school={card.school} program={card.program} dates={card.dates} key = {card.key}/>
+                                    <button className = 'material-icon' key = {card.key+'-button'}>edit</button> 
                                 </div>
                     })}
                     <div className = 'education-card'>
                         <form className = 'education-details'>
-                            <input placeholder = 'school'></input>
-                            <input placeholder = 'program'></input>
-                            <input placeholder = 'dates'></input>
+                            <input 
+                                type = 'text'
+                                placeholder = 'school' 
+                                name = 'school' 
+                                value={this.state.card.school}
+                                onChange = {this.handleChange}>
+                            </input>
+                            <input 
+                                type = 'text'   
+                                placeholder = 'program' 
+                                name = 'program'
+                                value={this.state.card.program}
+                                onChange = {this.handleChange}>
+                            </input>
+                            <input 
+                                type = 'text'   
+                                placeholder = 'dates' 
+                                name = 'dates' 
+                                value={this.state.card.dates}
+                                onChange = {this.handleChange}>
+                            </input>
                         </form>
-                        <button className = 'material-icon'>add_circle</button> 
+                        <button className = 'material-icon' onClick = {this.handleSubmit}>add_circle</button>   
                     </div>
                 </section>
         }
