4a656d1b5d416f3baab2a6d28b73e1d511126ce4
hoholabs <johnpaulhoran@gmail.com>
2022-09-07 19:33:52 -0400

    complete overhaul of Education section, since I now know how to pass data to parent components

--- src/components/Education.js
+++ src/components/Education.js
@@ -17,46 +17,17 @@ class Education extends Component{
             buttonLabel: 'edit',
         }
 
-        this.editEducation = this.editEducation.bind(this);
-        this.editCard = this.editCard.bind(this);
+        this.editSection = this.editSection.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
-    editEducation = () =>{
+    editSection = () =>{
         
         this.setState({
             editingSection:!this.state.editingSection,
             buttonLabel: this.state.buttonLabel === 'edit' ? 'check' : 'edit'
         })
-    }
-
-    editCard = (event, key) =>{
-
-        const cards = this.state.cards;
-        const index = cards.findIndex(card => {
-            return card.key === key;
-          });
-        const card = cards[index];
-
-        if(card.editingCard === true){
-            card.editingCard = false;
-        }else{
-            card.editingCard = true
-        }
-
-        cards[index]=card;
-
-        if(event.target.textContent === 'edit'){
-            event.target.textContent = 'check'
-        }else{
-            event.target.textContent = 'edit'
-        }
-
-        this.setState({
-            cards
-        });
-
     }
 
     handleChange = (event) =>{
@@ -75,7 +46,9 @@ class Education extends Component{
 
     handleSubmit = (event) =>{
         event.preventDefault();
+
         this.setState(prevState =>({
+
             cards:[...prevState.cards, this.state.card],
             card:{
                 school:'',
@@ -91,56 +64,21 @@ class Education extends Component{
 
         return  <section id = 'education'>  
                     <h1>Education</h1>
-                    <button className = 'material-icon' onClick = {this.editEducation}>{this.state.buttonLabel}</button> 
+                    <button className = 'material-icon' onClick = {this.editSection}>{this.state.buttonLabel}</button> 
 
                     {/* returns all of the education cards, using the state cards */}
                     {this.state.cards.map((card) =>{
-			            return  <div key = {card.key+'-container'} className = 'education-card'>
-                                    
-                                    {!card.editing &&
-                                    <EducationCard 
-                                        school={card.school} 
-                                        program={card.program} 
-                                        dates={card.dates} 
-                                        key={card.key} 
-                                        editingCard = {card.editingCard}/>
-                                    }
+			            return  <EducationCard card = {card} key={card.key}
+                                        editingSection = {this.state.editingSection}
+                                        handleChange = {this.handleChange}
+                                        handleSubmit = {this.handleSubmit}
+                                    />
+                            
+                        })}
 
-                                    {card.editing &&
-                                        <form className = 'education-details'>
-                                        <input 
-                                            type = 'text'
-                                            placeholder = 'school' 
-                                            name = 'school' 
-                                            value={this.state.card.school}
-                                            onChange = {this.handleChange}>
-                                        </input>
-                                        <input 
-                                            type = 'text'   
-                                            placeholder = 'program' 
-                                            name = 'program'
-                                            value={this.state.card.program}
-                                            onChange = {this.handleChange}>
-                                        </input>
-                                        <input 
-                                            type = 'text'   
-                                            placeholder = 'dates' 
-                                            name = 'dates' 
-                                            value={this.state.card.dates}
-                                            onChange = {this.handleChange}>
-                                        </input>
-                                    </form>
-                                    }
+                    {/* <EducationCard card = {this.state.card} key = {this.state.card.key}/> */}
 
-                                    {/* edit button */}
-                                    {this.state.editingSection && (
-                                        <button 
-                                        onClick = {event => this.editCard(event, card.key)} 
-                                        className = 'material-icon' 
-                                        key = {card.key+'-button'}
-                                        >edit</button>)}
-                                </div>
-                    })}
+                    {/* this shows 'new card' when the section is in edit mode */}
                     {this.state.editingSection && (
                         <div id = 'new-education-card' className = 'education-card'>
                             <form className = 'education-details'>
@@ -165,6 +103,7 @@ class Education extends Component{
                                     value={this.state.card.dates}
                                     onChange = {this.handleChange}>
                                 </input>
+                                
                             </form>
                             <button className = 'material-icon' onClick = {this.handleSubmit}>add_circle</button> 
                         </div>
--- src/components/EducationCard.js
+++ src/components/EducationCard.js
@@ -2,11 +2,43 @@ import React from 'react'
 
 const EducationCard = (props) =>{
 
-        return <ul className = 'education-details'>
-                <li className = 'school'>{props.school}</li>
-                <li className = 'program'>{props.program}</li>
-                <li className = 'dates'>{props.dates}</li>
-        </ul>
+        return (
+        !props.editingCard ? 
+                <ul className = 'education-card'>
+                <li className = 'school'>{props.card.school}</li>
+                <li className = 'program'>{props.card.program}</li>
+                <li className = 'dates'>{props.card.dates}</li>
+                {props.editingSection && <button className = 'material-icon' >edit</button>}
+                </ul> : 
+                
+                <form className = 'education-card'>
+                        <input 
+                        type = 'text'
+                        placeholder = 'school' 
+                        name = 'school' 
+                        value={props.card.school}
+                        onChange = {props.handleChange}>
+                        </input>
+
+                        <input 
+                        type = 'text'   
+                        placeholder = 'program' 
+                        name = 'program'
+                        value={props.card.program}
+                        onChange = {props.handleChange}>
+                        </input>
+
+                        <input 
+                        type = 'text'   
+                        placeholder = 'dates' 
+                        name = 'dates' 
+                        value={props.card.dates}
+                        onChange = {props.handleChange}>
+                        </input>
+
+                        {/* <button className = 'material-icon' onClick = {props.handleSubmit}>edit</button>  */}
+                </form>
+        )
     
 }
 
